928c40acfb7bc8c97b6f6fa09ec362f6b7d53de4
Sharkerl <sharkerl@outlook.com>
2026-05-30 10:25:14 +0800

    Merge branch 'main' of https://github.com/Sharkerl/Mizuki-Blog

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -14,24 +14,23 @@ export type DeviceCategory = Record<string, Device[]> & {
 };
 
 export const devicesData: DeviceCategory = {
-	OnePlus: [
+	Ipad: [
 		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.webp",
-			specs: "Gray / 16G + 1TB",
-			description:
-				"Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
+			name: "Ipad Air 12.9 M3",
+			image: "/images/device/Ipad_air_m3.png",
+			specs: "Gray / 8G + 256GB",
+			description: "iPadOS携手App, 事事溜到飞起",
+			link: "https://www.apple.com.cn/ipad-air/",
 		},
 	],
-	Router: [
-		{
-			name: "GL-MT3000",
-			image: "/images/device/mt3000.webp",
-			specs: "1000Mbps / 2.5G",
-			description:
-				"Portable WiFi 6 router suitable for business trips and home use.",
-			link: "https://www.gl-inet.cn/products/gl-mt3000/",
-		},
-	],
+	// MacbookPro: [
+	// 	{
+	// 		name: "GL-MT3000",
+	// 		image: "/images/device/mt3000.webp",
+	// 		specs: "1000Mbps / 2.5G",
+	// 		description:
+	// 			"Portable WiFi 6 router suitable for business trips and home use.",
+	// 		link: "https://www.gl-inet.cn/products/gl-mt3000/",
+	// 	},
+	// ],
 };
